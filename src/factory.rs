7dbc8715ad8819612e18f79806395c3ae736442f
Takayuki Maeda <takoyaki0316@gmail.com>
2020-09-02 04:27:23 +0900

    fix unit test

--- src/factory.rs
+++ src/factory.rs
@@ -61,6 +61,7 @@ where
         model
     }
 
+    #[doc(hidden)]
     pub fn build_n<O>(&'a self, n: u16, f: O) -> T
     where
         O: Fn(&mut T),
@@ -84,6 +85,7 @@ where
         list
     }
 
+    #[doc(hidden)]
     pub fn build_list_n<O>(&'a self, number: u16, n: u16, f: O) -> Vec<T>
     where
         O: Fn(&mut T),
@@ -96,166 +98,175 @@ where
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::factory::{new, sequence, sequence_a};
-//     use chrono::{NaiveDate, NaiveDateTime};
-//     use serde::{Deserialize, Serialize};
+#[cfg(test)]
+mod tests {
+    use crate::factory::{new, sequence, sequence_a};
+    use chrono::{NaiveDate, NaiveDateTime};
+    use serde::{Deserialize, Serialize};
 
-//     #[test]
-//     fn test_new() {
-//         #[derive(Serialize, Deserialize)]
-//         struct Post {
-//             id: u16,
-//             title: String,
-//             approved: bool,
-//             file: File,
-//             tags: Vec<Tag>,
-//             created_at: NaiveDateTime,
-//         }
+    #[test]
+    fn test_new() {
+        #[derive(Serialize, Deserialize)]
+        struct Post {
+            id: u16,
+            title: String,
+            approved: bool,
+            file: File,
+            tags: Vec<Tag>,
+            created_at: NaiveDateTime,
+        }
 
-//         #[derive(Serialize, Deserialize, Debug)]
-//         struct File {
-//             id: u16,
-//             path: String,
-//         }
+        #[derive(Serialize, Deserialize, Debug)]
+        struct File {
+            id: u16,
+            path: String,
+        }
 
-//         #[derive(Serialize, Deserialize, Debug)]
-//         struct Tag {
-//             id: u16,
-//             name: String,
-//         }
+        #[derive(Serialize, Deserialize, Debug)]
+        struct Tag {
+            id: u16,
+            name: String,
+        }
 
-//         impl Default for File {
-//             fn default() -> Self {
-//                 File {
-//                     id: 1,
-//                     path: "path/to/beaver.png".to_string(),
-//                 }
-//             }
-//         }
+        impl Default for File {
+            fn default() -> Self {
+                File {
+                    id: 1,
+                    path: "path/to/beaver.png".to_string(),
+                }
+            }
+        }
 
-//         impl Default for Tag {
-//             fn default() -> Self {
-//                 Tag {
-//                     id: 1,
-//                     name: "tag".to_string(),
-//                 }
-//             }
-//         }
+        impl Default for Tag {
+            fn default() -> Self {
+                Tag {
+                    id: 1,
+                    name: "tag".to_string(),
+                }
+            }
+        }
 
-//         impl Default for Post {
-//             fn default() -> Self {
-//                 Post {
-//                     id: 1,
-//                     title: "post".to_string(),
-//                     approved: true,
-//                     file: File::default(),
-//                     tags: vec![],
-//                     created_at: NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0),
-//                 }
-//             }
-//         }
+        impl Default for Post {
+            fn default() -> Self {
+                Post {
+                    id: 1,
+                    title: "post".to_string(),
+                    approved: true,
+                    file: File::default(),
+                    tags: vec![],
+                    created_at: NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0),
+                }
+            }
+        }
 
-//         impl PartialEq for Tag {
-//             fn eq(&self, other: &Self) -> bool {
-//                 self.id == other.id && self.name == other.name
-//             }
-//         }
+        impl PartialEq for Tag {
+            fn eq(&self, other: &Self) -> bool {
+                self.id == other.id && self.name == other.name
+            }
+        }
 
-//         impl PartialEq for File {
-//             fn eq(&self, other: &Self) -> bool {
-//                 self.id == other.id && self.path == other.path
-//             }
-//         }
+        impl PartialEq for File {
+            fn eq(&self, other: &Self) -> bool {
+                self.id == other.id && self.path == other.path
+            }
+        }
 
-//         let tag_factory = new(Tag::default(), |tag, n| {
-//             tag.id = n;
-//             tag.name = format!("tag-{}", n)
-//         });
+        let post_factory = new(
+            Post::default(),
+            Box::new(|post, n| {
+                post.id = n;
+                post.title = format!("post-{}", n);
+                post.approved = false;
+                post.file = File {
+                    id: n,
+                    path: format!("path/to/file-{}", n),
+                };
+                post.tags = vec![
+                    Tag {
+                        id: n,
+                        name: format!("tag-{}", n),
+                    },
+                    Tag {
+                        id: n + 1,
+                        name: format!("tag-{}", n + 1),
+                    },
+                    Tag {
+                        id: n + 2,
+                        name: format!("tag-{}", n + 2),
+                    },
+                ];
+                post.created_at = NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0)
+            }),
+        );
 
-//         let file_factory = new(File::default(), |file, n| {
-//             file.id = n;
-//             file.path = format!("path/to/file-{}", n)
-//         });
+        assert_eq!(
+            post_factory.model,
+            r#"{"id":1,"title":"post","approved":true,"file":{"id":1,"path":"path/to/beaver.png"},"tags":[],"created_at":"2020-01-01T00:00:00"}"#
+        );
+        assert_eq!(post_factory.sequence.get(), 1);
 
-//         let post_factory = new(Post::default(), |post, n| {
-//             post.id = n;
-//             post.title = format!("post-{}", n);
-//             post.approved = false;
-//             post.file = file_factory.build(|_| {});
-//             post.tags = tag_factory.build_list(3, |_| {});
-//             post.created_at = NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0)
-//         });
+        let mut post = Post::default();
+        let f = post_factory.gen_func;
+        f(&mut post, 1);
 
-//         assert_eq!(
-//             post_factory.model,
-//             r#"{"id":1,"title":"post","approved":true,"file":{"id":1,"path":"path/to/beaver.png"},"tags":[],"created_at":"2020-01-01T00:00:00"}"#
-//         );
-//         assert_eq!(post_factory.sequence.get(), 1);
+        assert_eq!(post.id, 1);
+        assert_eq!(post.title, "post-1");
+        assert_eq!(post.approved, false);
+        assert_eq!(
+            post.file,
+            File {
+                id: 1,
+                path: "path/to/file-1".to_string()
+            }
+        );
+        assert_eq!(
+            post.tags,
+            vec![
+                Tag {
+                    id: 1,
+                    name: "tag-1".to_string()
+                },
+                Tag {
+                    id: 2,
+                    name: "tag-2".to_string()
+                },
+                Tag {
+                    id: 3,
+                    name: "tag-3".to_string()
+                }
+            ]
+        );
+        assert_eq!(
+            post.created_at,
+            NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0)
+        );
+    }
 
-//         let mut post = Post::default();
-//         let f = post_factory.gen_func;
-//         f(&mut post, 1);
+    #[test]
+    fn test_sequence() {
+        assert_eq!(sequence(2, 1), 2);
+        assert_eq!(sequence(2, 2), 3);
+        assert_eq!(sequence(2, 3), 4);
+    }
 
-//         assert_eq!(post.id, 1);
-//         assert_eq!(post.title, "post-1");
-//         assert_eq!(post.approved, false);
-//         assert_eq!(
-//             post.file,
-//             File {
-//                 id: 1,
-//                 path: "path/to/file-1".to_string()
-//             }
-//         );
-//         assert_eq!(
-//             post.tags,
-//             vec![
-//                 Tag {
-//                     id: 1,
-//                     name: "tag-1".to_string()
-//                 },
-//                 Tag {
-//                     id: 2,
-//                     name: "tag-2".to_string()
-//                 },
-//                 Tag {
-//                     id: 3,
-//                     name: "tag-3".to_string()
-//                 }
-//             ]
-//         );
-//         assert_eq!(
-//             post.created_at,
-//             NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0)
-//         );
-//     }
+    #[test]
+    fn test_sequence_a() {
+        assert_eq!(sequence_a("a", 1), "a");
+        assert_eq!(sequence_a("a", 2), "b");
+        assert_eq!(sequence_a("a", 3), "c");
 
-//     #[test]
-//     fn test_sequence() {
-//         assert_eq!(sequence(2, 1), 2);
-//         assert_eq!(sequence(2, 2), 3);
-//         assert_eq!(sequence(2, 3), 4);
-//     }
+        assert_eq!(sequence_a("b", 1), "b");
+        assert_eq!(sequence_a("b", 2), "c");
+        assert_eq!(sequence_a("b", 3), "d");
 
-//     #[test]
-//     fn test_sequence_a() {
-//         assert_eq!(sequence_a("a", 1), "a");
-//         assert_eq!(sequence_a("a", 2), "b");
-//         assert_eq!(sequence_a("a", 3), "c");
+        assert_eq!(sequence_a("z", 1), "z");
+        assert_eq!(sequence_a("z", 2), "aa");
+        assert_eq!(sequence_a("z", 3), "ab");
 
-//         assert_eq!(sequence_a("b", 1), "b");
-//         assert_eq!(sequence_a("b", 2), "c");
-//         assert_eq!(sequence_a("b", 3), "d");
-
-//         assert_eq!(sequence_a("z", 1), "z");
-//         assert_eq!(sequence_a("z", 2), "aa");
-//         assert_eq!(sequence_a("z", 3), "ab");
-
-//         assert_eq!(sequence_a("z", 28), "ba");
-//         assert_eq!(sequence_a("z", 677), "zz");
-//         assert_eq!(sequence_a("z", 678), "aaa");
-//         assert_eq!(sequence_a("z", 975), "all");
-//         assert_eq!(sequence_a("z", 9975), "ntp");
-//     }
-// }
+        assert_eq!(sequence_a("z", 28), "ba");
+        assert_eq!(sequence_a("z", 677), "zz");
+        assert_eq!(sequence_a("z", 678), "aaa");
+        assert_eq!(sequence_a("z", 975), "all");
+        assert_eq!(sequence_a("z", 9975), "ntp");
+    }
+}
