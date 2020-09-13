a7e97a34e41e080e0774dd74ef5b00ffb282f0eb
Takayuki Maeda <takoyaki0316@gmail.com>
2020-09-14 00:03:08 +0900

    remove comments

--- examples/simple.rs
+++ examples/simple.rs
@@ -10,7 +10,6 @@ struct Post {
     created_at: NaiveDateTime,
 }
 
-// factory definition
 beaver::define! {
     PostFactory (Post) {
         id -> |n| n,
@@ -24,7 +23,6 @@ fn main() {
     let post_factory = PostFactory::new();
     let post1 = post_factory.build(|_| {});
     let post2 = post_factory.build(|_| {});
-    // overriding attributes of a factory
     let post3 = post_factory.build(|post| {
         post.id = 1024;
         post.title = "foo bar".to_string()
