ea1ef370a3ae9939b6652348e9ced79a254b319b
Takayuki Maeda <takoyaki0316@gmail.com>
2020-09-13 18:59:34 +0900

    remove a module

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,33 +1,25 @@
-use chrono::NaiveDateTime;
+use chrono::{NaiveDate, NaiveDateTime};
 use serde::{Deserialize, Serialize};
 
 // Post needs both of Serialize and Deserialize
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Post {
+struct Post {
     id: u16,
     title: String,
     approved: bool,
     created_at: NaiveDateTime,
 }
 
-mod factory {
-    use crate::Post;
-    use chrono::NaiveDate;
-
-    // factory definition
-    beaver::define! {
-        PostFactory (Post) {
-            id -> |n| n,
-            title -> |n| format!("{}", n),
-            approved -> |_| false,
-            created_at -> |_| NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0),
-        }
+beaver::define! {
+    PostFactory (Post) {
+        id -> |n| n,
+        title -> |n| format!("{}", n),
+        approved -> |_| false,
+        created_at -> |_| NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0),
     }
 }
 
 fn main() {
-    use factory::PostFactory;
-
     let post_factory = PostFactory::new();
     let post1 = post_factory.build(|_| {});
     let post2 = post_factory.build(|_| {});
