596f1d525f98414bca9f7f3778d1d90606188431
Takayuki Maeda <takoyaki0316@gmail.com>
2020-09-13 12:46:05 +0900

    use lazy_static

--- src/variable.rs
+++ src/variable.rs
@@ -1,35 +1,40 @@
-use phf::phf_map;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
 
 pub const ALPHABET: [&str; 26] = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
     "t", "u", "v", "w", "x", "y", "z",
 ];
 
-pub static ALPHABET_INDEX: phf::Map<&'static str, u8> = phf_map! {
-    "a" => 0,
-    "b" => 1,
-    "c" => 2,
-    "d" => 3,
-    "e" => 4,
-    "f" => 5,
-    "g" => 6,
-    "h" => 7,
-    "i" => 8,
-    "j" => 9,
-    "k" => 10,
-    "l" => 11,
-    "m" => 12,
-    "n" => 13,
-    "o" => 14,
-    "p" => 15,
-    "q" => 16,
-    "r" => 17,
-    "s" => 18,
-    "t" => 19,
-    "u" => 20,
-    "v" => 21,
-    "w" => 22,
-    "x" => 23,
-    "y" => 24,
-    "z" => 25,
-};
+lazy_static! {
+    pub static ref ALPHABET_INDEX: HashMap<&'static str, u8> = {
+        let mut m = HashMap::new();
+        m.insert("a", 0);
+        m.insert("b", 1);
+        m.insert("c", 2);
+        m.insert("d", 3);
+        m.insert("e", 4);
+        m.insert("f", 5);
+        m.insert("g", 6);
+        m.insert("h", 7);
+        m.insert("i", 8);
+        m.insert("j", 9);
+        m.insert("k", 10);
+        m.insert("l", 11);
+        m.insert("m", 12);
+        m.insert("n", 13);
+        m.insert("o", 14);
+        m.insert("p", 15);
+        m.insert("q", 16);
+        m.insert("r", 17);
+        m.insert("s", 18);
+        m.insert("t", 19);
+        m.insert("u", 20);
+        m.insert("v", 21);
+        m.insert("w", 22);
+        m.insert("x", 23);
+        m.insert("y", 24);
+        m.insert("z", 25);
+        m
+    };
+}
